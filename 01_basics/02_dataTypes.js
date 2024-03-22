1ebdd8b482e02064f7ff7e2a0255a50d486be9c1
CHETAN <142914310+Chetan291997@users.noreply.github.com>
2024-03-22 14:36:49 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict";  //treat all js code as newer version
+
+// alert("Hello")  // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high never write code like this
+
+console.log("Shiva")
+
+let name = "Shiva"  //string
+let age = 18   // Int
+let isLoggedIn = false //boolean
+let state;  
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//Boolean => true/false
+//null => standalone value
+// undefined => when variable is declared but not assigned a value
+//symbol => we use it to find unique thing 
+
+
+//object
+
+
+// console.log(typeof null);   =>  object
+// console.log(typeof undefined);   =>  undefined
